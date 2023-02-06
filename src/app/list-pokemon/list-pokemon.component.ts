1aae305d34676df4fc0791750988abc17f342ac4
haitham kossentini <haitham.kossentini@esprit.tn>
2023-02-06 21:38:44 +0100

    after cleaning v2

--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -9,21 +9,7 @@ import { POKEMONS } from '../mock-pokemon-list'
 })
 export class ListPokemonComponent  {
 
-  pokemonList: Pokemon[] = POKEMONS
-  pokemonSelected: Pokemon | undefined
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
 
-  selectPokemon(pokemonId: String) {
-
-    const id = +pokemonId
-    const pokemon: Pokemon | undefined = this.pokemonList.find(
-      (pokemon) => pokemon.id == id
-    )
-    if (pokemon) {
-      console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`)
-      this.pokemonSelected = pokemon
-    } else {
-      console.log(`Vous avez demandé un pokémon  qui n'existe pas`)
-      this.pokemonSelected = pokemon
-    }
-  }
 }
