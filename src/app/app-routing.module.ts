aead6cb1b024ef78a72c7e5441c9cc2ad651096f
haitham kossentini <haitham.kossentini@esprit.tn>
2023-02-06 21:34:20 +0100

    changer code de appComponent vers ListePokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
-  { path: '/pokemons', component: ListPokemonComponent },
-  { path: '/pokemon/:id ', component: DetailPokemonComponent },
-  { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemon/:id ', component: DetailPokemonComponent },
+  { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
 ]
 
 @NgModule({
--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core'
+import { Pokemon } from '../pokemon'
+//pour implementer ngOninit on doit la'ajouter à  coté de component
+import { Component, OnInit } from '@angular/core'
+import { POKEMONS } from '../mock-pokemon-list'
 
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: 'list-pokemon.component.html',
   styles: [],
 })
-export class ListPokemonComponent {}
+export class ListPokemonComponent implements OnInit {
+  //pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce']
+  //pokemonList c'est une liste de pokemons
+  pokemonList: Pokemon[] = POKEMONS
+  pokemonSelected: Pokemon | undefined
+  ngOnInit() {
+    //pour afficher un tableau console.table
+    console.table(this.pokemonList)
+    //this.selectPokemon(this.pokemonList[0])
+  }
+  selectPokemon(pokemonId: String) {
+    //en javascript null quand il est casté en nombre vaut 0
+    //+ pour transformer en nombre
+    //const index: number = +(event.target as HTMLInputElement).value
+    const id = +pokemonId
+    const pokemon: Pokemon | undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id == id
+    )
+    if (pokemon) {
+      console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`)
+      this.pokemonSelected = pokemon
+    } else {
+      console.log(`Vous avez demandé un pokémon  qui n'existe pas`)
+      this.pokemonSelected = pokemon
+    }
+  }
+}
