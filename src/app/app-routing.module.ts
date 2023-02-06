caa934288ed20576833dfc109c6d09e2bffa1e8b
haitham kossentini <haitham.kossentini@esprit.tn>
2023-02-06 22:50:38 +0100

    refactoring module pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component'
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { DetailPokemonComponent } from './pokemon/detail-pokemon/detail-pokemon.component'
+import { ListPokemonComponent } from './pokemon/list-pokemon/list-pokemon.component'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
@@ -7,6 +8,7 @@ const routes: Routes = [
   { path: 'pokemons', component: ListPokemonComponent },
   { path: 'pokemon/:id', component: DetailPokemonComponent },
   { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BorderCardDirective } from './pokemon/border-card.directive'
+import { PokemonTypeColorPipe } from './pokemon/pokemon-type-color.pipe'
+import { ListPokemonComponent } from './pokemon/list-pokemon/list-pokemon.component'
+import { DetailPokemonComponent } from './pokemon/detail-pokemon/detail-pokemon.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 @NgModule({
   declarations: [
@@ -14,13 +15,11 @@ import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.componen
     BorderCardDirective,
     PokemonTypeColorPipe,
     ListPokemonComponent,
-    DetailPokemonComponent
+    DetailPokemonComponent,
+    PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
