68c2e96d9cd182b9ee40ca733930c4b80d4d79d5
haitham kossentini <haitham.kossentini@esprit.tn>
2023-02-06 23:04:16 +0100

    refactoring module pokemon v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
-import { DetailPokemonComponent } from './pokemon/detail-pokemon/detail-pokemon.component'
-import { ListPokemonComponent } from './pokemon/list-pokemon/list-pokemon.component'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
-  { path: 'pokemons', component: ListPokemonComponent },
-  { path: 'pokemon/:id', component: DetailPokemonComponent },
   { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ]
