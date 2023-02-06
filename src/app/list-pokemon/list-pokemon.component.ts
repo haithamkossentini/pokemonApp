f1df2917f415ea7f3696b532350ad1b11ed33fb3
haitham kossentini <haitham.kossentini@esprit.tn>
2023-02-06 21:35:05 +0100

    after cleaning

--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,5 +1,4 @@
 import { Pokemon } from '../pokemon'
-//pour implementer ngOninit on doit la'ajouter à  coté de component
 import { Component, OnInit } from '@angular/core'
 import { POKEMONS } from '../mock-pokemon-list'
 
@@ -8,20 +7,13 @@ import { POKEMONS } from '../mock-pokemon-list'
   templateUrl: 'list-pokemon.component.html',
   styles: [],
 })
-export class ListPokemonComponent implements OnInit {
-  //pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce']
-  //pokemonList c'est une liste de pokemons
+export class ListPokemonComponent  {
+
   pokemonList: Pokemon[] = POKEMONS
   pokemonSelected: Pokemon | undefined
-  ngOnInit() {
-    //pour afficher un tableau console.table
-    console.table(this.pokemonList)
-    //this.selectPokemon(this.pokemonList[0])
-  }
+
   selectPokemon(pokemonId: String) {
-    //en javascript null quand il est casté en nombre vaut 0
-    //+ pour transformer en nombre
-    //const index: number = +(event.target as HTMLInputElement).value
+
     const id = +pokemonId
     const pokemon: Pokemon | undefined = this.pokemonList.find(
       (pokemon) => pokemon.id == id
